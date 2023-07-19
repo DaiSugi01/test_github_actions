00d5d07321beba18c859bd1144535076fcdd8fc8
Daiki Sugihara <volble124@gmail.com>
2023-07-19 21:51:48 +0900

    test

--- main_test.go
+++ main_test.go
@@ -1,10 +1,14 @@
 package test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	total := Add()
 	if total != 2 {
+		fmt.Println("a")
 		t.Errorf("Add(1, 2) = %d; want 3", total)
 	}
 }
